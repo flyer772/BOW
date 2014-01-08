f441f54dba0af63c99911b4444909685f3ca15ac
Cobi <cobim100@hotmail.com>
2014-01-07 19:27:22 -0500

    NavBar, Devise, Cancan, and Rolify

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BOW::Application.config.secret_key_base = '9168bf444f3977f8d885d71a043371b3870809d11107095b8c4fc940ae6255002ea76541bdd2c91238e157318587096cb86cbac01747f2e93f7c3a2a396954e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BOW::Application.config.session_store :cookie_store, key: '_BOW_session'
