7433fe6a4836959db4a5cdaa53322f124ab47c12
Cobi <cobim100@hotmail.com>
2014-01-09 21:28:51 -0500

    active class is used for the page currently on

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
-    def error_messages_for( object )
+   
+  def error_messages_for( object )
     render(:partial => 'layouts/error_messages', :locals => {:object => object})
   end
+  
+ def is_active?(link_path)
+  if current_page?(link_path)
+    "active"
+  else
+    ""
+  end
+ end
   
 end
